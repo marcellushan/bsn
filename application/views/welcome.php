3680671521c01b0a38c7f558eb1415798e698a59
Marc Hannah <mhannah@highlands.edu>
2017-10-30 10:43:14 -0400

    initial

---
+++ application/views/welcome.php
@@ -0,0 +1,36 @@
+<div class="main container">
+    <div class="row">
+        <h1 class="col-md-12" align="center">
+            Welcome to the Georgia Highlands College <br>RN/BSN Online Application
+        </h1>
+
+        <div class="col-md-10 col-md-offset-1">
+            <h2 class="mytitle">Application for Admission<br>
+                RN/BSN Program
+                <br>Fall 2017
+            </h2>
+            <h3>All applicants must meet the following requirements to be considered for admission to the program:</h3>
+            <h4>
+                <ul>
+                    <li>A current valid RN license to practice nursing in the U.S. with no current disciplinary actions.
+                        If practicing as an RN for four years or more, must submit a completed Verification of Employment form.</li>
+                    <li> Graduated from an accredited nursing program: National League for Nursing Accrediting Commission (NLNAC),
+                        Accreditation Commission for Education in Nursing (ACEN), or Commission on Collegiate Nursing Education (CCNE).</li>
+                    <li>A minimum cumulative GPA of 2.5 on a 4.0 scale.</li>
+                    <li>All Sciences courses, English 1102, and Math 2200 must be complete with a grade of "C" or better.</li>
+                </ul>
+            </h4>
+        </div><!-- col-md-12 -->
+    </div><!--row-->
+    <div class="row">
+<div class="col-sm-4 col-sm-offset-2"><button type="button"class="btn btn-lg btn-success"
+                                                                               onclick="window.location.href='<?=base_url("home/display/create_applicant")?>'">New Applicants</button></div>
+
+                    <div class="col-sm-4 col-sm-offset-2"><button type="button"class="btn btn-lg btn-success"
+                                                                  onclick="window.location.href='<?=base_url("home/login")?>'">Returning Applicants</button></div>
+
+        </div><!--col-sm-6 -->
+     </div><!--row -->
+    </div><!-- container -->
+
+
